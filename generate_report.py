6985098f4f972e4212d8f35a6de1bcfb6174d31e
lleonardus <leonardus.wizard@gmail.com>
2025-08-13 12:45:25 -0300

    criando mais algumas queries pra fazer testes

--- generate_report.py
+++ generate_report.py
@@ -21,7 +21,50 @@ try:
 
     top5_month_products = cursor.fetchall()
 
-    print(top5_month_products)
+    print(f"Top 5 month products:\n{top5_month_products}\n")
+
+    top_clients_sql = """
+    SELECT c.nome_cliente, SUM(pe.valor_total) total_gasto
+      FROM clientes c
+      JOIN pedidos pe ON pe.cliente_id = c.id
+      GROUP BY c.nome_cliente
+    ORDER BY total_gasto DESC, c.nome_cliente ASC;
+    """
+
+    cursor.execute(top_clients_sql)
+
+    top_clients = cursor.fetchall()
+
+    print(f"Top Clients:\n{top_clients}\n")
+
+    late_deliveries_sql = """
+    SELECT e.id, c.nome_cliente, e.data_prevista, e.data_entrega
+      FROM entregas e
+      JOIN pedidos pe ON  pe.id = e.pedido_id
+      JOIN clientes c ON  c.id = pe.cliente_id
+      WHERE data_entrega > data_prevista
+    ORDER BY data_entrega DESC;
+    """
+
+    cursor.execute(late_deliveries_sql)
+
+    late_deliveries = cursor.fetchall()
+
+    print(f"Late Deliveries:\n{late_deliveries}\n")
+
+    billing_by_state_sql = """
+    SELECT c.estado, SUM(pe.valor_total) AS faturamento
+      FROM clientes c 
+      JOIN pedidos pe ON c.id=pe.cliente_id
+      GROUP BY c.estado
+    ORDER BY faturamento DESC;
+    """
+
+    cursor.execute(billing_by_state_sql)
+
+    billing_by_state = cursor.fetchall()
+
+    print(f"Billing By State: \n{billing_by_state}\n")
 
     connection.commit()
 except connector.DatabaseError as e:
