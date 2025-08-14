a90c5a31b5b2916b6f5d241c58b1ed3c55740e65
lleonardus <leonardus.wizard@gmail.com>
2025-08-14 10:31:51 -0300

    encadeando cursor.execute().fetchall() ao invés de executar a query e depois fazer o fetchall

--- generate_report.py
+++ generate_report.py
@@ -10,33 +10,30 @@ try:
     connection = connector.connect("distribuidora.db")
     cursor = connection.cursor()
 
-    cursor.execute(
+    top5_month_products = cursor.execute(
         """
-    SELECT pr.nome_produto, SUM(i.quantidade) as total_vendido
+    SELECT pr.nome_produto, SUM(i.quantidade) AS total_vendido
       FROM produtos pr
       JOIN itens_pedido i ON pr.id=i.produto_id
-      JOIN pedidos pe on i.pedido_id=pe.id
+      JOIN pedidos pe ON i.pedido_id=pe.id
       WHERE strftime('%m', pe.data_pedido) = strftime('%m', date())
       GROUP BY pr.nome_produto
     ORDER BY total_vendido DESC, pr.nome_produto ASC LIMIT(5);
     """
-    )
+    ).fetchall()
 
-    top5_month_products = cursor.fetchall()
-
-    cursor.execute(
+    top_clients = cursor.execute(
         """
-    SELECT c.nome_cliente, SUM(pe.valor_total) total_gasto
+    SELECT c.nome_cliente, SUM(pe.valor_total) AS total_gasto
       FROM clientes c
       JOIN pedidos pe ON pe.cliente_id = c.id
       GROUP BY c.nome_cliente
     ORDER BY total_gasto DESC, c.nome_cliente ASC;
     """
-    )
+    ).fetchall()
 
-    top_clients = cursor.fetchall()
-
-    late_deliveries_sql = """
+    late_deliveries = cursor.execute(
+        """
     SELECT e.id, c.nome_cliente, e.data_prevista, e.data_entrega
       FROM entregas e
       JOIN pedidos pe ON  pe.id = e.pedido_id
@@ -44,22 +41,17 @@ try:
       WHERE data_entrega > data_prevista
     ORDER BY data_entrega DESC;
     """
+    ).fetchall()
 
-    cursor.execute(late_deliveries_sql)
-
-    late_deliveries = cursor.fetchall()
-
-    billing_by_state_sql = """
+    billing_by_state = cursor.execute(
+        """
     SELECT c.estado, SUM(pe.valor_total) AS faturamento
       FROM clientes c 
       JOIN pedidos pe ON c.id=pe.cliente_id
       GROUP BY c.estado
     ORDER BY faturamento DESC;
     """
-
-    cursor.execute(billing_by_state_sql)
-
-    billing_by_state = cursor.fetchall()
+    ).fetchall()
 
     data_frames = {
         "Top 5 Produtos do Mês": pd.DataFrame(
