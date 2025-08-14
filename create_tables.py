9b606b42f02d82877afa5cc0962470d17af4c341
lleonardus <leonardus.wizard@gmail.com>
2025-08-14 10:44:18 -0300

    colocando sql direto no execute ao invés de criar uma variável pra ele

--- create_tables.py
+++ create_tables.py
@@ -7,65 +7,65 @@ try:
     connection = connector.connect("distribuidora.db")
     cursor = connection.cursor()
 
-    clients_table = """
+    cursor.execute(
+        """
     CREATE TABLE clientes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_cliente TEXT NOT NULL,
-    cidade TEXT NOT NULL,
-    estado TEXT NOT NULL,
-    telefone TEXT
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome_cliente TEXT NOT NULL,
+        cidade TEXT NOT NULL,
+        estado TEXT NOT NULL,
+        telefone TEXT
     );
     """
+    )
 
-    cursor.execute(clients_table)
-
-    products_table = """
+    cursor.execute(
+        """
     CREATE TABLE produtos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_produto TEXT NOT NULL,
-    categoria TEXT NOT NULL,
-    preco_unitario REAL NOT NULL
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome_produto TEXT NOT NULL,
+        categoria TEXT NOT NULL,
+        preco_unitario REAL NOT NULL
     );
     """
+    )
 
-    cursor.execute(products_table)
-
-    orders_table = """
+    cursor.execute(
+        """
     CREATE TABLE pedidos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cliente_id INTEGER NOT NULL,
-    data_pedido DATE NOT NULL,
-    valor_total REAL NOT NULL,
-    FOREIGN KEY (cliente_id) REFERENCES clientes (id)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cliente_id INTEGER NOT NULL,
+        data_pedido DATE NOT NULL,
+        valor_total REAL NOT NULL,
+        FOREIGN KEY (cliente_id) REFERENCES clientes (id)
     );
     """
+    )
 
-    cursor.execute(orders_table)
-
-    ordered_items_table = """
+    cursor.execute(
+        """
     CREATE TABLE itens_pedido (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pedido_id INTEGER NOT NULL,
-    produto_id INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos (id),
-    FOREIGN KEY (produto_id) REFERENCES produtos (id)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        pedido_id INTEGER NOT NULL,
+        produto_id INTEGER NOT NULL,
+        quantidade INTEGER NOT NULL,
+        FOREIGN KEY (pedido_id) REFERENCES pedidos (id),
+        FOREIGN KEY (produto_id) REFERENCES produtos (id)
     );
     """
+    )
 
-    cursor.execute(ordered_items_table)
-
-    deliveries_table = """
+    cursor.execute(
+        """
     CREATE TABLE entregas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pedido_id INTEGER NOT NULL,
-    data_prevista DATE NOT NULL,
-    data_entrega DATE,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos (id)
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        pedido_id INTEGER NOT NULL,
+        data_prevista DATE NOT NULL,
+        data_entrega DATE,
+        FOREIGN KEY (pedido_id) REFERENCES pedidos (id)
     );
     """
-
-    cursor.execute(deliveries_table)
+    )
 
     connection.commit()
 except connector.DatabaseError as e:
